5f365e3a5e472e1a5136d32c5ea800ab2a81ee6c
Amy Zhang <amyz@usc.edu>
2023-11-29 11:31:29 -0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -24,7 +24,7 @@ export default function Home() {
       <div className="mb-32 p-6 gap-8 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-5 lg:text-left">
         <a
           // href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          href="/table"
+          href="/studyHall"
           className="group lg:col-span-3 lg:row-span-2 rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800 dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
@@ -70,7 +70,8 @@ export default function Home() {
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          // href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="discover"
           className="group lg:col-span-2 lg:row-span-2 rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800 dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
