77a66d6c17e6753d57154fb86346198f72b0ab8c
Jeremy Yiu <jeremyyiu.student@gmail.com>
2023-11-29 04:48:31 -0800

    Completed Discover & StudyHall page.

--- app/discover/page.jsx
+++ app/discover/page.jsx
@@ -5,31 +5,36 @@ import Nav from '../../components/nav'
 import '../globals.css'
 
 export default function discoverPage() {
-  const data = [
-    { uid: 1, fName: 'Jeremy', lName: "Yiu", gpa: 1, studyHabits: "I don't"},
-    { uid: 2, fName: 'Amy', lName: "Zhang", gpa: 5, studyHabits: "I don't" },
-    { uid: 3, fName: 'Borun', lName: "Xu", gpa: 5, studyHabits: "I don't" },
-    { uid: 4, fName: 'David', lName: "Yang", gpa: 5, studyHabits: "I don't" },
-    { uid: 5, fName: 'Kelly', lName: "Yu", gpa: 5, studyHabits: "I don't" },
-    { uid: 6, fName: 'Shirley', lName: "Yu", gpa: 5, studyHabits: "I don't" },
-    { uid: 7, fName: 'Zhenyu', lName: "Zhao", gpa: 5, studyHabits: "I don't" },
-    { uid: 8, fName: 'Yuki', lName: "Yu", gpa: 5, studyHabits: "I don't" },
-  ]
-  const [users, setUsers] = useState(data)
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  if(users.length == 0){
+    postData("http://localhost:8080/TemporaryServer/fetchDiscover").then(
+    (out) => {
+      console.log(out);
+      setUsers(out);
+      console.log(users);
+      setLoading(false);
+    }
+  );
+  }
 
   const renderUsers = () => {
-    return users.map(({ uid, fName, lName, gpa, studyHabits }) => {
-      return <tr key={uid} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-      <td class="px-6 py- text-lg">{fName}</td>
-      <td class="px-6 py- text-lg">{lName}</td>
-      <td class="px-6 py-4 text-lg">{gpa}</td>
-      <td class="px-6 py-4 text-lg">{studyHabits}</td>
-      <td class="px-6 py-4 text-lg">
-        <button type="button" onClick={(e) => {
-          requestButton({uid})}} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" value={uid}>Request</button>
-      </td>
-    </tr>
-    })
+    if(loading){
+      return (<div>Loading...</div>);
+    }else{
+      return users.map(({ uid, fName, lName, gpa, studyHabits }) => {
+        return <tr key={uid} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <td class="px-6 py- text-lg">{fName}</td>
+        <td class="px-6 py- text-lg">{lName}</td>
+        <td class="px-6 py-4 text-lg">{gpa}</td>
+        <td class="px-6 py-4 text-lg">{studyHabits}</td>
+        <td class="px-6 py-4 text-lg">
+          <button type="button" onClick={(e) => {
+            requestButton({uid})}} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" value={uid}>Request</button>
+        </td>
+      </tr>
+      })
+    }
   }
   
   const renderTable = () => {
@@ -42,38 +47,32 @@ export default function discoverPage() {
           <th scope="col" class="px-6 py-3 text-lg">Study Habits</th>
           <th class="px-6 py-3"></th>
         </thead>
-        <tbody>
-          {renderUsers()}
-        </tbody>
+        <tbody>{renderUsers()}</tbody>
       </table>
     )
   }
   return (<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
     <Nav></Nav><div class="relative overflow-x-auto">{renderTable()}</div></main>)
 }
-
-function requestButton(id){
-  postData("http://localhost:8080/TemporaryServer/submitForm", id);
-}
 async function postData(url = "", data = {}) {
     const response = await fetch(url, {
         method: "POST",
-        mode: "no-cors",
         cache: "no-cache",
         credentials: "same-origin",
         headers: {"Content-Type": "application/json",},
         redirect: "follow",
         referrerPolicy: "no-referrer",
         body: JSON.stringify(data),
-    }).then((res) => {
-      alert(res);
-      res.text()
-      .then((json) => 
-      alert("Sending a request to " + json)
-    )});
-    return response.json();
+    });
+    const result = await response.json();
+    if(result.state == "success"){
+      console.log(result.token);
+      return result.token;
+    }else{
+      alert("Cannot fetch");
+    }
 }
 
-/**
- * 
- */
+async function requestButton(uid) {
+  postData("http://localhost:8080/TemporaryServer/friendRequest", uid);
+}
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -21,8 +21,7 @@ export default function Login() {
         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
           Password
         </label>
-        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" placeholder="******************"/>
-        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" placeholder="******************"/>
       </div>
       <div class="flex items-center justify-between">
         <button type="button" class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={(e) => {
