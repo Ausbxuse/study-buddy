76519d423aadf65eb771a31f01cee7fc948624bf
Jeremy Yiu <jeremyyiu.student@gmail.com>
2023-11-30 23:57:16 -0800

    Mock Data

--- backend/database/chatroom.sql
+++ backend/database/chatroom.sql
@@ -56,3 +56,25 @@ CREATE TABLE Friends(
     FOREIGN KEY (UID) REFERENCES Users(UID),
     FOREIGN KEY (FUID) REFERENCES Users(UID)
 );
+
+--Below is mock data
+INSERT INTO Users (USERNAME, PASSWORD, FIRSTNAME, LASTNAME, PREFNAME, SHORTTERM, LONGTERM, GPA, YEAR)
+VALUES 
+('johndoe', 'password123', 'John', 'Doe', 'Johnny', TRUE, FALSE, 3.5, 2),
+('janedoe', 'securepass', 'Jane', 'Doe', 'Janey', FALSE, TRUE, 3.8, 1),
+('mikebrown', 'mikepass', 'Michael', 'Brown', 'Mike', TRUE, TRUE, 2.7, 3),
+('lucygreen', 'lucy1234', 'Lucy', 'Green', 'Luc', FALSE, TRUE, 3.9, 4),
+('davidlee', 'davidpw', 'David', 'Lee', 'Dave', TRUE, FALSE, 2.9, 2),
+('emilywhite', 'emilypw', 'Emily', 'White', 'Em', FALSE, TRUE, 3.6, 1),
+('samsmith', 'ssmith', 'Samuel', 'Smith', 'Sam', TRUE, FALSE, 3.3, 3),
+('lilyevans', 'lilypassword', 'Lily', 'Evans', 'Lils', FALSE, TRUE, 3.7, 4),
+('marktwain', 'marktwainpw', 'Mark', 'Twain', 'MarkT', TRUE, TRUE, 2.8, 2),
+('sarahconnor', 'sarahcpass', 'Sarah', 'Connor', 'SarahC', FALSE, TRUE, 3.4, 1);
+
+INSERT INTO Classes (NAME) VALUES ('CSCI201'), ('CSCI104'), ('CSCI103'), ('CSCI170'), ('CSCI1270'), ('CSCI310'), ('CSCI401');
+
+INSERT INTO Enrollments (UID, CID) VALUES (1, 1), (2, 1), (2, 2), (3, 2), (3, 3);
+
+INSERT INTO Friends(UID, FUID) VALUES (1,2), (1,4), (1,6), (1, 8), (1,10);
+
+INSERT INTO FriendRequests(SENDER, RECEIVER) VALUES (3, 1), (5, 1), (7, 1), (9, 1);
