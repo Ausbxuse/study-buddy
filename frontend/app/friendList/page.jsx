83f1912e4b4fd025a4befb7577de351bb4c3d4bd
Jeremy Yiu <jeremyyiu.student@gmail.com>
2023-11-30 16:30:05 -0800

    Updated Friends Page

--- frontend/app/friendList/page.jsx
+++ frontend/app/friendList/page.jsx
@@ -6,9 +6,11 @@ import '../globals.css'
 
 export default function friendList() {
   const [users, setUsers] = useState([]);
+  const [requests, setRequests] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [loading2, setLoading2] = useState(false);
   if(users.length == 0){
-    postData("http://localhost:8080/TemporaryServer/fetchDiscover").then(
+    postData("http://localhost:8080/TemporaryServer/fetchFriends").then(
     (out) => {
       console.log(out);
       setUsers(out);
@@ -17,20 +19,39 @@ export default function friendList() {
     }
   );
   }
+  if(requests.length == 0){
+    postData("http://localhost:8080/TemporaryServer/fetchFriendRequests").then(
+    (out) => {
+      console.log(out);
+      setRequests(out);
+      console.log(users);
+      setLoading2(false);
+    }
+  );
+  }
 
   const renderUsers = () => {
     if(loading){
       return (<div>Loading...</div>);
     }else{
-      return users.map(({ uid, fName, lName, gpa, studyHabits }) => {
+      return users.map(({ uid, fName, lName, status}) => {
         return <tr key={uid} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-        <td class="px-6 py- text-lg">{fName}</td>
-        <td class="px-6 py- text-lg">{lName}</td>
-        <td class="px-6 py-4 text-lg">{gpa}</td>
-        <td class="px-6 py-4 text-lg">{studyHabits}</td>
+        <td class="px-6 py- text-lg">{fName} {lName}</td>
+        <td class="px-6 py- text-lg">{status}</td>
+      </tr>
+      })
+    }
+  }
+  const renderUsers2 = () => {
+    if(loading){
+      return (<div>Loading...</div>);
+    }else{
+      return users.map(({ uid, fName, lName}) => {
+        return <tr key={uid} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <td class="px-6 py- text-lg">{fName} {lName}</td>
         <td class="px-6 py-4 text-lg">
           <button type="button" onClick={(e) => {
-            requestButton({uid})}} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" value={uid}>Request</button>
+            acceptButton({uid})}} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" value={uid}>Accept</button>
         </td>
       </tr>
       })
@@ -39,20 +60,38 @@ export default function friendList() {
   
   const renderTable = () => {
     return (
+      <>
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <th scope="col" class="px-6 py-3 text-lg">First Name</th>
-          <th scope="col" class="px-6 py-3 text-lg">Last Name</th>
-          <th scope="col" class="px-6 py-3 text-lg">GPA</th>
-          <th scope="col" class="px-6 py-3 text-lg">Study Habits</th>
-          <th class="px-6 py-3"></th>
+          <th scope="col" class="px-6 py-3 text-lg">Name</th>
+          <th scope="col" class="px-6 py-3 text-lg">Status</th>
         </thead>
         <tbody>{renderUsers()}</tbody>
       </table>
+      </>
+    )
+  }
+
+  const renderTable2 = () => {
+    return (
+      <>
+      <h2 class=" text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 px-6 py-3 text-lg">Incoming Friend Requests</h2>
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <th scope="col" class="px-6 py-3 text-lg">Name</th>
+          <th scope="col" class="px-6 py-3 text-lg">Accept?</th>
+        </thead>
+        <tbody>{renderUsers2()}</tbody>
+      </table>
+      </>
     )
   }
   return (<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-    <Nav></Nav><div class="relative overflow-x-auto">{renderTable()}</div></main>)
+    <Nav></Nav>
+    <div class="relative overflow-x-auto grid grid-cols-2">
+      <div>{renderTable()}</div>
+      <div>{renderTable2()}</div>
+    </div></main>)
 }
 async function postData(url = "", data = {}) {
     const response = await fetch(url, {
@@ -73,6 +112,6 @@ async function postData(url = "", data = {}) {
     }
 }
 
-async function requestButton(uid) {
-  postData("http://localhost:8080/TemporaryServer/friendRequest", uid);
+async function acceptButton(uid) {
+  postData("http://localhost:8080/TemporaryServer/acceptRequest", uid);
 }
