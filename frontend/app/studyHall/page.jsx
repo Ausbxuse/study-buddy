44a0de51489664231bc95f4218495dc059a6e958
Jeremy Yiu <jeremyyiu.student@gmail.com>
2023-12-01 18:25:52 -0800

    Update to Chatroom Schema

--- frontend/app/studyHall/page.jsx
+++ frontend/app/studyHall/page.jsx
@@ -22,11 +22,9 @@ export default function studyHall() {
     if(loading){
       return (<div>Loading...</div>);
     }else{
-      return users.map(({ CID, CNAME, CREATOR, COUNT, CAPACITY }) => {
+      return users.map(({ CID, NAME}) => {
         return <tr key={CID} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-        <td class="px-6 py- text-lg">{CNAME}</td>
-        <td class="px-6 py- text-lg">{CREATOR}</td>
-        <td class="px-6 py-4 text-lg">{COUNT}/{CAPACITY}</td>
+        <td class="px-6 py- text-lg">{NAME}</td>
         <td class="px-6 py-4 text-lg">
           <button type="button" onClick={(e) => {
             location.href= "/chatroom/"+{CID}.CID}} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" value={CID}>Join</button>
@@ -41,8 +39,6 @@ export default function studyHall() {
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <th scope="col" class="px-6 py-3 text-lg">Chatroom</th>
-          <th scope="col" class="px-6 py-3 text-lg">Creator</th>
-          <th scope="col" class="px-6 py-3 text-lg">Capacity</th>
           <th class="px-6 py-3"></th>
         </thead>
         <tbody>{renderChatroom()}</tbody>
